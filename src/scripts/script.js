f20abbc1e4f67fa9b75a14ab21daca3d2907e409
aapanasov <aapanasov@gmail.com>
2022-05-07 11:46:14 +0300

    feat: add hint text

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,11 +1,17 @@
 import Keyboard from './keyboard.js';
 
+// TODO: hint
+const hint = document.createElement('div');
+hint.classList.add('hint');
+hint.innerText = 'Клавиатура создана в операционной системе Windows\nДля переключения языка комбинация: левыe ctrl + shift';
+document.body.append(hint);
+
 // TODO: textarea
 const textarea = document.createElement('textarea');
 textarea.setAttribute('id', 'textarea');
 document.body.append(textarea);
 
-// TODO: main
+// TODO: keyboard
 
 const lang = localStorage.getItem('lang') || 'en';
 const keyboard = new Keyboard(textarea, lang);
